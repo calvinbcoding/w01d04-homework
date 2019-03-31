0f5e9fd8bb202af46573b3f04f242e7f0f97b1a4
Calvin Bruch <calvinbcoding@gmail.com>
2019-03-31 11:59:16 -0600

    "Verbal Questions"

---
+++ js/app.js
@@ -0,0 +1,20 @@
+
+
+
+
+//1.) Verbal Questions:
+//		1.) In a JS function, a parameter is a varable used to define 
+//			the function while an argument is a piece of data that is 
+//			passed into the function. 
+//		2.) conse.log(); is like a function within that function; it's 
+//			purpose is to log a value in the console (it also serves the
+// 			purpose of debugging the function). Return is the result of a
+// 			function when it is finished; console.log doesn't stop the function
+//			from continueing to run--return does. 
+// 		3.) If you can write a function to return a value then you are asking
+// 			the computer to actually compute something. Without functions 
+//			(code that serves to give a defined output for a given input value)
+//			the computer wouldn't be computing anything. It would basically
+//			just be a database storing values written out by coders.
+//			
+			
