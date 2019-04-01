e1aebe35e2b31452c2512dffab7e9137e308eab3
Calvin Bruch <calvinbcoding@gmail.com>
2019-04-01 16:28:09 -0600

    "pythagoras finished"

--- README.md
+++ README.md
@@ -101,6 +101,14 @@ To test whether a number is Prime, you only need to test as far as the **square 
 ### Step One
 Write a function called `checkPrime` that will test whether a number is Prime. The function will return true (Boolean) if Prime, false if not.
 _Hint:_ Check every number up to the square root. To do this, try a _for loop_.
+function checkPrime (num){
+    for(1=2; i< num; i++){
+        if (num % i === 0) {
+            return true;
+        else{ return false};
+        }
+    }
+}
 
 ### Step Two
 Write another function called `printPrimes` that will print (console log) all the Primes up to an arbitrary limit. For example, if you invoke your function with `printPrimes(97)`, it will print all the Prime numbers up to and including 97.
--- js/app.js
+++ js/app.js
@@ -18,75 +18,69 @@
 //			just be a database storing values written out by coders.
 //			
 			
-//2.)
-// function isPalindrome(s,i) {
-// 	return (i=i||0)<0||i>=s.length>>1||s[i]==s[s.length-1-i]&&isPalindrome(s,++i);
-//    }
-
-// const checkPalindrome = function(str)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
-// 	{if(str === str.split('').reverse().join('').toUpperCase());
-// 	return true, else                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     ddcccxcccc false};
-// 	(str !== str.split('').reverse().join('').toUpperCase());
-// 	return false};
-// console.log(checkPalindrome('Radar'));
-// console.log(checkPalindrome('Borscht'));
-//let checkPalindrome(str) = str.split('').reverse().join('');
-	//console.log("Radar".split('').reverse().join(''));
-		
-
-
-
-// 	const reverseString('') =>
-// 	for(str = ('Radar'))
-// 	{str.split('').reverse().join('') }; 
-// 	const split = str.split('');
-// 	const reverse = (str.split('')).reverse();
-// 	const join = reverse.join('');
-//     console.log(reverseString());
-// }};
-// reverseString("hello");
-
-
-// const checkPalindrome(str) => 
-// 	if(str.split('').reverse().join('')){
-// 		return trueconsole.log(true)
+// //2.)
+// const checkPalindrome = (str) =>{
+// 	str.toUpperCase();{
+// 		for(let i=0; i < str.length; i ++){
+// 			if(str[i] !== str[str.length -1 -i]){
+// 				return false 
+// 			}else{
+// 				return true
+// 			}
+// 		}
 // 	}
-const checkPalindrome = (str) =>{
-	str.toUpperCase();{
-		for(let i=0; i < str.length; i ++){
-			if(str[i] !== str[str.length -1 -i]){
-				return false 
-			}else{
-				return true
-			}
-		}
-	}
-}
-console.log(checkPalindrome("radar"));
-console.log(checkPalindrome("Radar"));
-console.log(checkPalindrome("Borscht"));
-// const checkPalindrome = function(str) {
-// 	str.split('').reverse().join('')};
-// checkPalindrome('Radar');
-		// console.log(checkPalindrome('Radar'));
-	// const splitPal = str.split('');
-	// 	console.log(splitPal.toLower);
-	// const reverse = splitPal.reverse();
-	// 	console.log(reverse);
-	// const join = reverse.join('');
-	// 	console.log(join);
-	// 	console.log(join.toUpperCase());
-	// 	console.log(str.split('').reverse().join(''));                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
-// // const checkPalindrome(str);
-// // 	if (str == splitPal.reverse.join);
-// console.log(splitPal.reverse.join);
-// // 	if (str === str.split('').reverse().join('');
-// 		console.log('');
-// 	else(console.log(false));
 // }
-// const checkPalindrome(str) = str.split('').reverse().join('');
-	//     str.split('').reverse().join('')};
-	// console.log(checkPalindrome(str));
-	// //         return true;
-	// //  		else return false;
-	// // }
+// console.log(checkPalindrome("radar"));
+// console.log(checkPalindrome("Radar"));
+// console.log(checkPalindrome("Borscht"));
+
+//#3.)
+
+
+//4.) Pythagoras 
+	function pythagoras(sideA, sideB) {
+		let sideC = ((sideA * sideA) + (sideB * sideB));
+		let hypo = Math.sqrt(sideC) 
+		return hypo;
+	};
+	console.log(pythagoras(3, 4));
+
+	
+
+
+
+// // function pythagoras([arr]) {
+// // 	let total = 0
+
+// //}
+
+
+
+
+
+
+
+// //5)
+// // let prime = true;
+// 	function isPrime(num) { 
+// 		for(let i = 2; i < num; i++) {
+// 			if(num % i === 0) {
+// 				return false;
+// 			};
+// 		};
+// 		return num > 1;
+// 	};
+// printPrime[i]{
+// 	for(i=2; i < 97; i++){
+// 	if(isPrime === true){
+// 		console.log(i)
+// 	}
+// }
+// } = console.log(isPrime(11));
+// 	console.log(isPrime(20));
+// 	console.log(isPrime(11));
+   
+	
+	
+
+
