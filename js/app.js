8305e137429ce966fdb46b01cbcef9d93dd114c5
Calvin Bruch <calvinbcoding@gmail.com>
2019-04-01 09:30:53 -0600

    "tried #2 with a loop, can't return capitalized"

--- js/app.js
+++ js/app.js
@@ -19,35 +19,74 @@
 //			
 			
 //2.)
+// function isPalindrome(s,i) {
+// 	return (i=i||0)<0||i>=s.length>>1||s[i]==s[s.length-1-i]&&isPalindrome(s,++i);
+//    }
+
+// const checkPalindrome = function(str)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
+// 	{if(str === str.split('').reverse().join('').toUpperCase());
+// 	return true, else                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     ddcccxcccc false};
+// 	(str !== str.split('').reverse().join('').toUpperCase());
+// 	return false};
+// console.log(checkPalindrome('Radar'));
+// console.log(checkPalindrome('Borscht'));
+//let checkPalindrome(str) = str.split('').reverse().join('');
+	//console.log("Radar".split('').reverse().join(''));
+		
 
 
-const str = 'Radar';
-	console.log('Radar');
-const splitPal = str.split('');
-	console.log(splitPal);
-const reverse = splitPal.reverse();
-	console.log(reverse);
-const join = reverse.join('');
-	console.log(join);
-	console.log(join.toUpperCase());
 
-// const checkPalindrome(str);
-// 	if (str == splitPalindrome.reverse.join);
+// 	const reverseString('') =>
+// 	for(str = ('Radar'))
+// 	{str.split('').reverse().join('') }; 
+// 	const split = str.split('');
+// 	const reverse = (str.split('')).reverse();
+// 	const join = reverse.join('');
+//     console.log(reverseString());
+// }};
+// reverseString("hello");
 
-console.log(splitPal.reverse.join);
 
-// 	if (str === str.split('').reverse().join('');
+// const checkPalindrome(str) => 
+// 	if(str.split('').reverse().join('')){
+// 		return trueconsole.log(true)
+// 	}
+const checkPalindrome = (str) =>{
+	str.toUpperCase();{
+		for(let i=0; i < str.length; i ++){
+			if(str[i] !== str[str.length -1 -i]){
+				return false 
+			}else{
+				return true
+			}
+		}
+	}
+}
+console.log(checkPalindrome("radar"));
+console.log(checkPalindrome("Radar"));
+console.log(checkPalindrome("Borscht"));
+// const checkPalindrome = function(str) {
+// 	str.split('').reverse().join('')};
+// checkPalindrome('Radar');
+		// console.log(checkPalindrome('Radar'));
+	// const splitPal = str.split('');
+	// 	console.log(splitPal.toLower);
+	// const reverse = splitPal.reverse();
+	// 	console.log(reverse);
+	// const join = reverse.join('');
+	// 	console.log(join);
+	// 	console.log(join.toUpperCase());
+	// 	console.log(str.split('').reverse().join(''));                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+// // const checkPalindrome(str);
+// // 	if (str == splitPal.reverse.join);
+// console.log(splitPal.reverse.join);
+// // 	if (str === str.split('').reverse().join('');
 // 		console.log('');
 // 	else(console.log(false));
 // }
-
 // const checkPalindrome(str) = str.split('').reverse().join('');
-
-
-
 	//     str.split('').reverse().join('')};
 	// console.log(checkPalindrome(str));
 	// //         return true;
 	// //  		else return false;
 	// // }
-	
